5377926ccac74aceb49b33e7c8b945c3b4e70900
MarcoDeRosso <mcoder@hotmail.it>
2021-07-08 14:19:38 +0200

    index + vue.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+Vue.config.devtools = true;
+
+new Vue({
+    el: '#app',
+    data: {
+        emails: []
+    },
+    created() {
+        this.getEmail()
+    },
+    methods: {
+
+        getEmail: function() {
+            for (let i = 0; i < 5; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.emails.push(response.data.response);
+                    });
+            }
+        }
+
+    }
+});
