8ca5b5b300989e88522ea2d90ac0151f4cc89501
MarcoDeRosso <mcoder@hotmail.it>
2021-07-08 14:27:12 +0200

    v-if for show data together

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ new Vue({
     methods: {
 
         getEmail: function() {
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then((response) => {
                         this.emails.push(response.data.response);
